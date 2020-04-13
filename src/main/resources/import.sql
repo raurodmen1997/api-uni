81e4939b3b7d13dc78d7e9716d3b5cb073a5ea00
crilorbre <43986572+crilorbre@users.noreply.github.com>
2020-04-13 16:30:03 +0200

    añadido algunos datos al import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,19 @@
+INSERT INTO `universidades` (`nombre`) VALUES ('Universidad de Sevilla');
+
+INSERT INTO `facultades` (`nombre`, `universidad_id`) VALUES ('Escuela Técnica Superior de Ingeniería Informática', '1');
+INSERT INTO `facultades` (`nombre`, `universidad_id`) VALUES ('Escuela Técnica Superior de Arquitectura', '1');
+INSERT INTO `facultades` (`nombre`, `universidad_id`) VALUES ('Escuela Técnica Superior de Ingeniería de Edificación', '1');
+INSERT INTO `facultades` (`nombre`, `universidad_id`) VALUES ('Facultad de Matemática', '1');
+INSERT INTO `facultades` (`nombre`, `universidad_id`) VALUES ('Facultad de Biología', '1');
+INSERT INTO `facultades` (`nombre`, `universidad_id`) VALUES ('Facultad de Farmacia', '1');
+INSERT INTO `facultades` (`nombre`, `universidad_id`) VALUES ('Facultad de Física', '1');
+INSERT INTO `facultades` (`nombre`, `universidad_id`) VALUES ('Facultad de Química', '1');
+
+INSERT INTO `grados` (`nombre`, `facultad_id`, `numerocursos`) VALUES ('Ingeniería del Software', '1', '4');
+INSERT INTO `grados` (`nombre`, `facultad_id`, `numerocursos`) VALUES ('Ingeniería de Computadores', '1', '4');
+INSERT INTO `grados` (`nombre`, `facultad_id`, `numerocursos`) VALUES ('Tecnología informática', '1', '4');
+INSERT INTO `grados` (`nombre`, `facultad_id`, `numerocursos`) VALUES ('Ingeniería de la salud', '1', '4');
+
 INSERT INTO `cursos` (`nombre`) VALUES ('PRIMERO');
 INSERT INTO `cursos` (`nombre`) VALUES ('SEGUNDO');
 INSERT INTO `cursos` (`nombre`) VALUES ('TERCERO');
